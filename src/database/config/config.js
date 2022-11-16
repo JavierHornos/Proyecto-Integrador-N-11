cbb2705a707558c6dc13c15145f4e899632375fc
JavierHornos <hjavih@gmail.com>
2022-11-16 05:56:08 -0300

    creando env y codigos en heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,24 +1,14 @@
+require('dotenv').config();
+
 module.exports = {
+
   "development": {
-    "username": "288635_digital",
-    "password": "GrupoN11",
-    "database": "digital-drinks_db",
-    "host": "mysql-digital-drinks.alwaysdata.net",
-    "dialect": "mysql",
-    "port": 3306
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "port":process.env.DB_PORT
   },
-  "test": {
-    "username": "288635_digital",
-    "password": "GrupoN11",
-    "database": "digital-drinks_db",
-    "host": "mysql-digital-drinks.alwaysdata.net",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "288635_digital",
-    "password": "GrupoN11",
-    "database": "digital-drinks_db",
-    "host": "mysql-digital-drinks.alwaysdata.net",
-    "dialect": "mysql"
-  }
+ 
 }
