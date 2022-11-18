067aad6802d7e7d89b940afc28edc746edaaa490
lauranuneztech <lauranunez1412@gmail.com>
2022-11-17 21:03:56 -0300

    contrasena y confirmar contrasena tienen el ojito y se ven las contrasenas

--- public/js/validador.js
+++ public/js/validador.js
@@ -1,8 +1,10 @@
-alert ("saludos desde el front-end!");
+
 
 const password= document.getElementById("password")
 
-const confirmaContrasena= document.getElementById("comfirm_password")
+const confirmaContrasena= document.getElementById("password2")
+
+//funciones//
 
 function validarContrasena(){
     if (password.value == confirmaContrasena.value){
@@ -13,6 +15,27 @@ function validarContrasena(){
     }
 }
 
+function mostrarPassword1() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+  function mostrarPassword2() {
+    var x = document.getElementById("password2");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+}
+
+//eventos//
 password.onchange= validarContrasena
 
 confirmaContrasena.onkeyup= validarContrasena
+
+
