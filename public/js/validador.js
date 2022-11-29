decc8066ac362fad7032e7f31cfe695ed5ebd014
lucasvallone <lucasvallone@gmail.com>
2022-11-29 17:59:12 -0300

    cambios  vista registro

--- public/js/validador.js
+++ public/js/validador.js
@@ -14,23 +14,55 @@ function validarContrasena(){
 }
 
 function mostrarPassword1() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
+    var x = document.getElementById("Password");
+    if (x.type === "Password") {
       x.type = "text";
     } else {
-      x.type = "password";
+      x.type = "Password";
     }
   }
 
   function mostrarPassword2() {
     var x = document.getElementById("password2");
-    if (x.type === "password") {
+    if (x.type === "Password") {
       x.type = "text";
     } else {
-      x.type = "password";
+      x.type = "Password";
     }
 }
 
+
+window.addEventListener("load", function(){
+  let formulario = document.querySelector("#formulario-registro");
+  let campoNombre = document.querySelector("#nombre");
+  console.log(campoNombre)
+
+  console.log(formulario)
+
+campoNombre.addEventListener("blur", function(){
+  if(campoNombre.value == ""){
+      alert("El campo de nombre debe estar completo")
+  }
+})
+
+  formulario.addEventListener("submit", function(e){
+      e.preventDefault();
+
+      let campoNombre = document.querySelector("#nombre");
+console.log(campoNombre)
+
+      if(campoNombre.value == ""){
+          alert("El campo de nombre debe estar completo")
+      }
+
+      else if(campoNombre.value.length < 4){
+          alert("El campo de nombre debe tener al menos 4 caracteres")
+      }
+
+
+  })
+
+})
 
 //eventos//
 password.onchange= validarContrasena
--- src/middlewares/validacionRegistro.js
+++ src/middlewares/validacionRegistro.js
@@ -10,7 +10,7 @@ module.exports =  [
     .exists().withMessage('Completar el campo password')
         .isAlphanumeric().isLength({mnin: 4}).withMessage('La contraseña debe tener al menos 4 caracteres'),
 
-    body('Nombre')
+    body('nombre')
     .exists().withMessage('Completar el campo nombre')
         .isLength({mnin: 4}).withMessage('el Nombre debe tener al menos 4 caracteres'),
 
