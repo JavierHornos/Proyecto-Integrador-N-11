75dbd7eb4123acbbd71799c4f12848f282674e38
lauranuneztech <lauranunez1412@gmail.com>
2022-12-03 11:18:05 -0300

    validar editar productos campos y mostrar mensaje sweet alerts

--- public/js/editar-producto.js
+++ public/js/editar-producto.js
@@ -1,3 +1,48 @@
+let formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit",function(e) {
+   e.preventDefault();
+
+   let image = document.getElementById("imagen").value
+   let description = document.getElementById("description").value.trim()
+   let category = document.getElementById("category").value
+   let price = document.getElementById("price").value.trim()
+   let name = document.getElementById("name").value.trim()
+
+   if (image == "") {
+      return Swal.fire({
+         icon: 'error',
+         title: 'Validacion',
+         text: 'Cargue una imagen!'
+      });
+   } else if (description == "") {
+      return Swal.fire({
+         icon: 'error',
+         title: 'Validacion',
+         text: 'Agregue una descripcion!'
+      });
+   } else if (category == "") {
+      return Swal.fire({
+         icon: 'error',
+         title: 'Validacion',
+         text: 'Seleccione una categoria!'
+      });
+   } else if (price == "") {
+      return Swal.fire({
+         icon: 'error',
+         title: 'Validacion',
+         text: 'Debe asignar un precio para el producto!'
+      });
+   } else if (name == "") {
+      return Swal.fire({
+         icon: 'error',
+         title: 'Validacion',
+         text: 'Todo producto debe tener un nombre!'
+      });
+   } else {
+      formulario.submit();
+   } 
+});
 
  document.getElementById('imagen').onchange=function(e){
     let reader=new FileReader();
