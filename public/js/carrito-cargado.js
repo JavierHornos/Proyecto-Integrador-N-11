6dbfd675b1a887b7b0c43e89e418aa79e49bc2a8
FranciscoValenza <franciscovalenza@gmail.com>
2022-12-14 00:08:35 -0300

    carrito cargado con estilo y casi con imagen

--- public/js/carrito-cargado.js
+++ public/js/carrito-cargado.js
@@ -1,3 +1,5 @@
+
+let imagen = document.getElementById("imagen");
 let nombre = document.getElementById("nombre");
 // console.log(nombre)
 let precio = document.getElementById("precio");
@@ -15,9 +17,30 @@ if (localStorage.carrito) {
     let carrito = JSON.parse(localStorage.carrito);
     //console.log(carrito)
 
+
+
+       carrito.forEach(e => {
+        let div = document.createElement("div")
+        div.style.borderTop = "gainsboro solid 3px"
+        div.style.height = "50px"
+        div.style.display = "flex"
+        div.style.alignItems ="center"
+        div.style.justifyContent ="center"
+        e.imagen
+        div.innerHTML = '<div class="columna1"><img src="/imagenes/productos/<%=e.imagen%>" class="jack-daniels" ></img></div>'
+        imagen.appendChild(div)
+       })
+
    
+       
+
    carrito.forEach(e => {
     let div = document.createElement("div")
+    div.style.borderTop = "gainsboro solid 3px"
+    div.style.height = "50px"
+    div.style.display = "flex"
+    div.style.alignItems ="center"
+    div.style.justifyContent ="center"
     div.innerHTML = e.nombre
     nombre.appendChild(div)
    })
@@ -25,26 +48,42 @@ if (localStorage.carrito) {
 
    carrito.forEach(e => {
     let div = document.createElement("div")
-    div.innerHTML = e.precio  
+    div.style.borderTop = "gainsboro solid 3px"
+    div.style.height = "50px"
+    div.style.display = "flex"
+    div.style.alignItems ="center"
+    div.style.justifyContent ="center"
+    div.innerHTML = "$" + e.precio  
     precio.appendChild(div)
    })
 
    carrito.forEach(e => {
     let div = document.createElement("div")
+    div.style.borderTop = "gainsboro solid 3px"
+    div.style.height = "50px"
+    div.style.display = "flex"
+    div.style.alignItems ="center"
+    div.style.justifyContent ="center"
     div.innerHTML = e.Cantidad
     cantidad.appendChild(div)
    })
 
    carrito.forEach(e => {
     let div = document.createElement("div")
-    div.innerHTML = e.Cantidad * e.precio
+    div.style.borderTop = "gainsboro solid 3px"
+    div.style.height = "50px"
+    div.style.display = "flex"
+    div.style.alignItems ="center"
+    div.style.justifyContent ="center"
+    div.innerHTML = "$" + e.Cantidad * e.precio
     total.appendChild(div)
    })
 
  
 let sumaTotal = carrito.reduce((acum, prod) => acum + prod.precio * prod.Cantidad, 0)
 let div = document.createElement("div")
-div.innerHTML = sumaTotal
+div.style.fontSize = "x-large"
+div.innerHTML = "$" + sumaTotal
 total2.appendChild(div)
 
 
