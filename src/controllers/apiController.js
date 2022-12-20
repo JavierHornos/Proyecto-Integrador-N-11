76b466269b3c4bcec0f825bb7fa0992da878fea7
JavierHornos <hjavih@gmail.com>
2022-12-20 00:01:46 -0300

    apis

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -3,65 +3,53 @@ const db = require("../database/models");
 module.exports = {
   usuarios: async function (req, res) {
     await db.usuarios
-        .findAll()
-        .then((listaUsuarios) => {
-            return res.status(200).json({
-                total: listaUsuarios.length,
-                 data: listaUsuarios,
-                status: 200,
-            });
+      .findAll({attributes: {exclude: ["Password"]}})
+      .then((listaUsuarios) => {
+        return res.status(200).json({
+          total: listaUsuarios.length,
+          data: listaUsuarios,
+          status: 200,
         });
-    },
+      });
+  },
 
   usuarioDetalle: async function (req, res) {
     await db.usuarios
-        .findByPk(req.params.id)
-        .then((usuario) => {
-            return res.status(200).json({
-                data: usuario,
-                status: 200,
-
-            });
-         });
-    },
+      .findByPk(req.params.id, {attributes: {exclude: ["Password"]}})
+      .then((usuario) => {
+        return res.status(200).json({
+          data: usuario,
+          status: 200,
+        });
+      });
+  },
 
   lista: async function (req, res) {
-    await db.productos
-        .findAll()
-        .then((listaProductos) => {
-            return res.status(200).json({
-                total: listaProductos.length,
-                data: listaProductos,
-                status: 200,
-            });
-        });
-    },
+    await db.productos.findAll().then((listaProductos) => {
+      return res.status(200).json({
+        total: listaProductos.length,
+        data: listaProductos,
+        status: 200,
+      });
+    });
+  },
 
+  productosDetalle: async function (req, res) {
+    await db.productos.findByPk(req.params.id).then((producto) => {
+      return res.status(200).json({
+        data: producto,
+        status: 200,
+      });
+    });
+  },
 
-    productosDetalle: async function (req, res) {
-        await db.productos
-            .findByPk(req.params.id)
-            .then((producto) => {
-                return res.status(200).json({
-                    data: producto,
-                    status: 200,
-    
-                });
-             });
-        },
-
-  
-        categorias: async function (req, res) {
-            await db.categorias
-            .findAll()
-            .then((listaProductos) => {
-                return res.status(200).json({
-                    total: listaProductos.length,
-                    data: listaProductos,
-                    status: 200,
-                });
-            });
-            },
-        
-        
+  categorias: async function (req, res) {
+    await db.categorias.findAll().then((listaProductos) => {
+      return res.status(200).json({
+        total: listaProductos.length,
+        data: listaProductos,
+        status: 200,
+      });
+    });
+  },
 };
